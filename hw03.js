4bbd92a360b807dae8c88ee200dd8ac3bebb030c
REEMA <reema.rayes@gmail.com>
2019-02-27 13:10:24 +0300

    Assignment03

---
+++ hw03.js
@@ -0,0 +1,48 @@
+/*
+console.log("Currency Converter:")
+var amount = parseInt(prompt("Please enter the required amount"));
+var currency = prompt("Please enter the required currency (USD, GBP, EGP, BD)");
+
+
+if (currency == "USD"){
+    console.log(amount+ "in Saudi Riyal equals to "+ (amount * 3.75))
+}
+if (currency == "GBP"){
+    console.log(amount+ "in Saudi Riyal equals to "+ (amount * 4.5))
+}
+if (currency == "EGP"){
+    console.log(amount+ "in Saudi Riyal equals to "+ (amount * 0.5))
+}
+if (currency == "BD"){
+    console.log(amount+ "in Saudi Riyal equals to "+ (amount * 0.10))
+}
+else {
+  console.log("Please enter a valid currency")
+}
+
+*/
+
+
+
+/*
+console.log("is Character A Vowel:")
+//var vowel = function(vowel) 
+parseInt(prompt("Please enter a word"));
+if (vowel.includes('a', 'e' ,'u' ,'i', 'o')){
+     return true
+}
+else {
+    return false
+}
+*/
+
+
+
+/*
+console.log("Pow")
+  function pow(base, exponent) 
+    console.log(Math.pow())
+
+ pow(2, 2)
+ 
+ */
