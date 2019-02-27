d4198488237a96d2a05255b0563c13bb9ca11e4f
rehabsaadoun <YOUR GITHUB EMAIL>
2019-02-27 09:46:14 +0300

    work-1

---
+++ answers.js
@@ -0,0 +1,54 @@
+var amount, currency, result;
+
+currency = window.promet("enter type of currency","");
+
+amount = window.prompt("enter the volue","");
+
+amount= parseFloat(amount);
+
+currencyConverter(amount,currency);
+
+
+
+
+function currencyConverter (amount,currency){
+
+if (currency =="usa"){
+
+result = amount *3.75;
+
+return result;}
+
+
+
+else if (currency=="GBP"){
+
+result = amount *4.97;
+
+return result;}
+
+var charactor
+charactor= window.promet("enter one charactor","");
+
+function is charocter AVowel(character){
+    if charactoer="O" // character="o" //charactore="U"// character"u"
+    //character="A"//charctore="a"//character="I"//character="i"//character"E"//charcter="e"
+    {returne true;}
+    else{
+    returne false;}
+
+    var base,exponet,
+    var result,
+    base= window.promet("enter base number","");
+    exponet= window.promet("enter exponet number","");
+
+    pow(base,exponet);
+    function pow (base,exponet);
+    result= Math.pow (base,exponet);
+    document.write(result);
+}
+
+
+
+
+
